675dc38f7e69c287c789f320412cc92f5d27ab7e
Dylan Ham <dylanham@gmail.com>
2015-07-18 20:07:30 -0600

    user can crud journals

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,4 +1,9 @@
 class JournalsController < ApplicationController
+  before_action :set_journal, only: [:show, :edit, :update]
+
+  def show
+  end
+
   def new
     @journal = Journal.new
   end
@@ -14,14 +19,33 @@ class JournalsController < ApplicationController
     end
   end
 
-  def show
-    @journal = Journal.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @journal.update(journal_params)
+      flash[:notice] = "you have successfully updated your journal"
+      redirect_to journal_path(@journal)
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    journal = Journal.find(params[:id])
+    journal.destroy
+    flash[:notice] = "You have Erased your Journal"
+    redirect_to root_path
   end
 
   private
 
   def journal_params
     params.require(:journal).permit(:name)
+  end
+
+  def set_journal
+    @journal = Journal.find(params[:id])
   end
 
 end
